538a236e783fde61d341fc31fd32b13fbede92bf
cleverNishanth <nishanthsoffcial@gmail.com>
2021-04-30 01:12:50 +0530

    first commit

---
+++ src/context/mockData.js/mockRepos.js
@@ -0,0 +1,268 @@
+export default [
+  {
+    id: 360013277,
+    node_id: 'MDEwOlJlcG9zaXRvcnkzNjAwMTMyNzc=',
+    name: 'react-netflix-clone',
+    full_name: 'cleverNishanth/react-netflix-clone',
+    private: false,
+    owner: {
+      login: 'cleverNishanth',
+      id: 75757846,
+      node_id: 'MDQ6VXNlcjc1NzU3ODQ2',
+      avatar_url: 'https://avatars.githubusercontent.com/u/75757846?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/cleverNishanth',
+      html_url: 'https://github.com/cleverNishanth',
+      followers_url: 'https://api.github.com/users/cleverNishanth/followers',
+      following_url:
+        'https://api.github.com/users/cleverNishanth/following{/other_user}',
+      gists_url: 'https://api.github.com/users/cleverNishanth/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/cleverNishanth/starred{/owner}{/repo}',
+      subscriptions_url:
+        'https://api.github.com/users/cleverNishanth/subscriptions',
+      organizations_url: 'https://api.github.com/users/cleverNishanth/orgs',
+      repos_url: 'https://api.github.com/users/cleverNishanth/repos',
+      events_url:
+        'https://api.github.com/users/cleverNishanth/events{/privacy}',
+      received_events_url:
+        'https://api.github.com/users/cleverNishanth/received_events',
+      type: 'User',
+      site_admin: false,
+    },
+    html_url: 'https://github.com/cleverNishanth/react-netflix-clone',
+    description: null,
+    fork: false,
+    url: 'https://api.github.com/repos/cleverNishanth/react-netflix-clone',
+    forks_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/forks',
+    keys_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/keys{/key_id}',
+    collaborators_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/collaborators{/collaborator}',
+    teams_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/teams',
+    hooks_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/hooks',
+    issue_events_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/issues/events{/number}',
+    events_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/events',
+    assignees_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/assignees{/user}',
+    branches_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/branches{/branch}',
+    tags_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/tags',
+    blobs_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/git/blobs{/sha}',
+    git_tags_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/git/tags{/sha}',
+    git_refs_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/git/refs{/sha}',
+    trees_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/git/trees{/sha}',
+    statuses_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/statuses/{sha}',
+    languages_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/languages',
+    stargazers_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/stargazers',
+    contributors_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/contributors',
+    subscribers_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/subscribers',
+    subscription_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/subscription',
+    commits_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/commits{/sha}',
+    git_commits_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/git/commits{/sha}',
+    comments_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/comments{/number}',
+    issue_comment_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/issues/comments{/number}',
+    contents_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/contents/{+path}',
+    compare_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/compare/{base}...{head}',
+    merges_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/merges',
+    archive_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/{archive_format}{/ref}',
+    downloads_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/downloads',
+    issues_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/issues{/number}',
+    pulls_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/pulls{/number}',
+    milestones_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/milestones{/number}',
+    notifications_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/notifications{?since,all,participating}',
+    labels_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/labels{/name}',
+    releases_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/releases{/id}',
+    deployments_url:
+      'https://api.github.com/repos/cleverNishanth/react-netflix-clone/deployments',
+    created_at: '2021-04-21T02:49:15Z',
+    updated_at: '2021-04-28T16:34:44Z',
+    pushed_at: '2021-04-21T02:58:05Z',
+    git_url: 'git://github.com/cleverNishanth/react-netflix-clone.git',
+    ssh_url: 'git@github.com:cleverNishanth/react-netflix-clone.git',
+    clone_url: 'https://github.com/cleverNishanth/react-netflix-clone.git',
+    svn_url: 'https://github.com/cleverNishanth/react-netflix-clone',
+    homepage: null,
+    size: 254,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: 'JavaScript',
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: 'main',
+  },
+  {
+    id: 356635789,
+    node_id: 'MDEwOlJlcG9zaXRvcnkzNTY2MzU3ODk=',
+    name: 'tea-station',
+    full_name: 'cleverNishanth/tea-station',
+    private: false,
+    owner: {
+      login: 'cleverNishanth',
+      id: 75757846,
+      node_id: 'MDQ6VXNlcjc1NzU3ODQ2',
+      avatar_url: 'https://avatars.githubusercontent.com/u/75757846?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/cleverNishanth',
+      html_url: 'https://github.com/cleverNishanth',
+      followers_url: 'https://api.github.com/users/cleverNishanth/followers',
+      following_url:
+        'https://api.github.com/users/cleverNishanth/following{/other_user}',
+      gists_url: 'https://api.github.com/users/cleverNishanth/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/cleverNishanth/starred{/owner}{/repo}',
+      subscriptions_url:
+        'https://api.github.com/users/cleverNishanth/subscriptions',
+      organizations_url: 'https://api.github.com/users/cleverNishanth/orgs',
+      repos_url: 'https://api.github.com/users/cleverNishanth/repos',
+      events_url:
+        'https://api.github.com/users/cleverNishanth/events{/privacy}',
+      received_events_url:
+        'https://api.github.com/users/cleverNishanth/received_events',
+      type: 'User',
+      site_admin: false,
+    },
+    html_url: 'https://github.com/cleverNishanth/tea-station',
+    description: null,
+    fork: false,
+    url: 'https://api.github.com/repos/cleverNishanth/tea-station',
+    forks_url: 'https://api.github.com/repos/cleverNishanth/tea-station/forks',
+    keys_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/keys{/key_id}',
+    collaborators_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/collaborators{/collaborator}',
+    teams_url: 'https://api.github.com/repos/cleverNishanth/tea-station/teams',
+    hooks_url: 'https://api.github.com/repos/cleverNishanth/tea-station/hooks',
+    issue_events_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/issues/events{/number}',
+    events_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/events',
+    assignees_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/assignees{/user}',
+    branches_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/branches{/branch}',
+    tags_url: 'https://api.github.com/repos/cleverNishanth/tea-station/tags',
+    blobs_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/git/blobs{/sha}',
+    git_tags_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/git/tags{/sha}',
+    git_refs_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/git/refs{/sha}',
+    trees_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/git/trees{/sha}',
+    statuses_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/statuses/{sha}',
+    languages_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/languages',
+    stargazers_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/stargazers',
+    contributors_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/contributors',
+    subscribers_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/subscribers',
+    subscription_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/subscription',
+    commits_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/commits{/sha}',
+    git_commits_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/git/commits{/sha}',
+    comments_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/comments{/number}',
+    issue_comment_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/issues/comments{/number}',
+    contents_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/contents/{+path}',
+    compare_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/compare/{base}...{head}',
+    merges_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/merges',
+    archive_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/{archive_format}{/ref}',
+    downloads_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/downloads',
+    issues_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/issues{/number}',
+    pulls_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/pulls{/number}',
+    milestones_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/milestones{/number}',
+    notifications_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/notifications{?since,all,participating}',
+    labels_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/labels{/name}',
+    releases_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/releases{/id}',
+    deployments_url:
+      'https://api.github.com/repos/cleverNishanth/tea-station/deployments',
+    created_at: '2021-04-10T16:23:32Z',
+    updated_at: '2021-04-16T09:27:47Z',
+    pushed_at: '2021-04-16T09:27:45Z',
+    git_url: 'git://github.com/cleverNishanth/tea-station.git',
+    ssh_url: 'git@github.com:cleverNishanth/tea-station.git',
+    clone_url: 'https://github.com/cleverNishanth/tea-station.git',
+    svn_url: 'https://github.com/cleverNishanth/tea-station',
+    homepage: null,
+    size: 3516,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: 'SCSS',
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: 'main',
+  },
+]
---
+++ src/context/mockData.js/mockUser.js
@@ -0,0 +1,38 @@
+export default {
+  login: 'cleverNishanth',
+  id: 75757846,
+  node_id: 'MDQ6VXNlcjc1NzU3ODQ2',
+  avatar_url: 'https://avatars.githubusercontent.com/u/75757846?v=4',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/cleverNishanth',
+  html_url: 'https://github.com/cleverNishanth',
+  followers_url: 'https://api.github.com/users/cleverNishanth/followers',
+  following_url:
+    'https://api.github.com/users/cleverNishanth/following{/other_user}',
+  gists_url: 'https://api.github.com/users/cleverNishanth/gists{/gist_id}',
+  starred_url:
+    'https://api.github.com/users/cleverNishanth/starred{/owner}{/repo}',
+  subscriptions_url:
+    'https://api.github.com/users/cleverNishanth/subscriptions',
+  organizations_url: 'https://api.github.com/users/cleverNishanth/orgs',
+  repos_url: 'https://api.github.com/users/cleverNishanth/repos',
+  events_url: 'https://api.github.com/users/cleverNishanth/events{/privacy}',
+  received_events_url:
+    'https://api.github.com/users/cleverNishanth/received_events',
+  type: 'User',
+  site_admin: false,
+  name: 'cleverNishanth',
+  company: null,
+  blog: '',
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 2,
+  public_gists: 0,
+  followers: 0,
+  following: 1,
+  created_at: '2020-12-09T21:09:12Z',
+  updated_at: '2021-04-29T00:51:19Z',
+}
